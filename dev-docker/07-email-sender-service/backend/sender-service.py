d61d3c39a4edae3c5e0061ae622c00940f199a6e
Rafael Cabral <rafaelsantanacabral@gmail.coma>
2020-10-17 21:56:45 -0300

    feat: database integration and network config

--- dev-docker/07-email-sender-service/backend/sender-service.py
+++ dev-docker/07-email-sender-service/backend/sender-service.py
@@ -1,4 +1,18 @@
+import psycopg2
 from flask import Flask, request, jsonify
+
+DSN = 'dbname=email_sender user=postgres host=mydbserver'
+SQL = 'INSERT INTO emails (subject, message) VALUES (%s, %s)'
+
+def register_message(subject, message):
+  db_Connection = psycopg2.connect(DSN)
+  cursor = db_Connection.cursor()
+  cursor.execute(SQL, (subject, message));
+  db_Connection.commit()
+  cursor.close()
+  db_Connection.close()
+
+  print("Message has been registred")
 
 app = Flask(__name__);
 
@@ -6,7 +20,10 @@ app = Flask(__name__);
 def sendMessage():
   subject = request.form['subject']
   message = request.form['message']
-  return jsonify(subject=subject, message=message)
+
+  register_message(subject, message)
+
+  return jsonify(message='Message has been sent!!!')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=8080, debug=True)
